73e6882c07ece2d23b0030afa716ab266ecc3873
mtaketani113 <47836003+mtaketani113@users.noreply.github.com>
2023-05-24 23:57:24 +0900

    formatterを実行

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { Menu } from "semantic-ui-react";
+import { useState } from 'react';
+import { Menu } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 /**
@@ -9,27 +9,22 @@ import { Link } from 'react-router-dom';
  * @returns
  */
 const Header = () => {
+  const [activeItem, setActiveItem] = useState<string>('home');
 
-    const [activeItem, setActiveItem] = useState<string>('home');
-    
-    return (
-        <Menu pointing>
-          <Menu.Item
-            name='home'
-            active={activeItem === 'home'}
-            onClick={() => setActiveItem('home')}
-          />
-          <Menu.Item
-            name='yaml2json'
-            active={activeItem === 'yamlAndJson'}
-            onClick={() => setActiveItem('yamlAndJson')}
-            as={Link}
-            to='yaml2json'
-          >yaml ⇔ json
-            </Menu.Item>
-        </Menu>
-    )
-
-}
+  return (
+    <Menu pointing>
+      <Menu.Item name="home" active={activeItem === 'home'} onClick={() => setActiveItem('home')} />
+      <Menu.Item
+        name="yaml2json"
+        active={activeItem === 'yamlAndJson'}
+        onClick={() => setActiveItem('yamlAndJson')}
+        as={Link}
+        to="yaml2json"
+      >
+        yaml ⇔ json
+      </Menu.Item>
+    </Menu>
+  );
+};
 
 export default Header;
--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'semantic-ui-css/semantic.min.css'
+import 'semantic-ui-css/semantic.min.css';
 import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter  basename={'/react-tools'}>
-    <App />
+    <BrowserRouter basename={'/react-tools'}>
+      <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
