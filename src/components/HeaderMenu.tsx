1633c383efa620a6623f2b6d6d5fed228f3081eb
mtaketani113 <47836003+mtaketani113@users.noreply.github.com>
2023-05-26 02:10:07 +0900

    URLで判定するように修正

--- src/components/HeaderMenu.tsx
+++ src/components/HeaderMenu.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Menu } from 'semantic-ui-react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 /**
  * ヘッダー部のJSX
@@ -9,21 +9,27 @@ import { Link } from 'react-router-dom';
  * @returns
  */
 const HeaderMenu = () => {
-  const [activeItem, setActiveItem] = useState<string>('home');
+  const [activeItem, setActiveItem] = useState<string>('/');
+
+  const location = useLocation();
+  useEffect(() => {
+    setActiveItem(location.pathname);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <Menu pointing>
       <Menu.Item
         name="home"
-        active={activeItem === 'home'}
-        onClick={() => setActiveItem('home')}
+        active={activeItem === '/'}
+        onClick={() => setActiveItem('/')}
         as={Link}
         to="/"
       />
       <Menu.Item
         name="json2yaml"
-        active={activeItem === 'json2yaml'}
-        onClick={() => setActiveItem('json2yaml')}
+        active={activeItem === '/json2yaml'}
+        onClick={() => setActiveItem('/json2yaml')}
         as={Link}
         to="json2yaml"
       >
