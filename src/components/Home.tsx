77524b3514e344da4d3a34614b70119c53407f98
mtaketani113 <47836003+mtaketani113@users.noreply.github.com>
2023-05-27 02:14:50 +0900

    fomatterを実行

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -10,17 +10,17 @@ import { Container } from 'semantic-ui-react';
 const Home = () => {
   return (
     <>
-    <HelmetProvider>
-      <Helmet>
-        <title>JavaScript Tools</title>
-      </Helmet>
-    </HelmetProvider>
-    <Container textAlign="left">
-      <li>JavaScriptのツール集です。Reactで作成しています。（作成中）</li>
-      <li>
-        <a href="https://react.semantic-ui.com/">semantic ui react</a>を利用しています。
-      </li>
-    </Container>
+      <HelmetProvider>
+        <Helmet>
+          <title>JavaScript Tools</title>
+        </Helmet>
+      </HelmetProvider>
+      <Container textAlign="left">
+        <li>JavaScriptのツール集です。Reactで作成しています。（作成中）</li>
+        <li>
+          <a href="https://react.semantic-ui.com/">semantic ui react</a>を利用しています。
+        </li>
+      </Container>
     </>
   );
 };
