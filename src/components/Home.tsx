1cfc1150c886255c43fa1bcc5ac09e2d5bc0a64f
mtaketani113 <47836003+mtaketani113@users.noreply.github.com>
2023-05-27 02:14:18 +0900

    title修正

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,3 +1,4 @@
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { Container } from 'semantic-ui-react';
 
 /**
@@ -8,12 +9,19 @@ import { Container } from 'semantic-ui-react';
  */
 const Home = () => {
   return (
+    <>
+    <HelmetProvider>
+      <Helmet>
+        <title>JavaScript Tools</title>
+      </Helmet>
+    </HelmetProvider>
     <Container textAlign="left">
       <li>JavaScriptのツール集です。Reactで作成しています。（作成中）</li>
       <li>
         <a href="https://react.semantic-ui.com/">semantic ui react</a>を利用しています。
       </li>
     </Container>
+    </>
   );
 };
 
