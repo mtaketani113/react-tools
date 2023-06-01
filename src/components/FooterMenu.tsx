630f44c8a33bb393561982e9b7a3dad843f33ca8
mtaketani113 <47836003+mtaketani113@users.noreply.github.com>
2023-06-01 17:29:47 +0900

    リンク追加

--- src/components/FooterMenu.tsx
+++ src/components/FooterMenu.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { Container, List, Segment } from 'semantic-ui-react';
+import { Container, Icon, List, Segment } from 'semantic-ui-react';
 
 /**
  * フッター部のJSX
@@ -11,7 +11,7 @@ const FooterMenu = () => {
   return (
     <Segment inverted vertical style={{ margin: '5em 0em 0em', padding: '5em 0em' }}>
       <Container textAlign="center">
-        <List horizontal inverted divided link size="small">
+        <List horizontal inverted divided link size="small" style={{ marginRight: '100px' }}>
           <List.Item as={Link} to="/inquiry">
             Contact Us
           </List.Item>
@@ -19,6 +19,15 @@ const FooterMenu = () => {
             Privacy Policy
           </List.Item>
         </List>
+        <List horizontal inverted link size="huge">
+          <List.Item as="a" href="https://twitter.com/mtaketani113">
+            <Icon name='twitter' />
+          </List.Item>
+          <List.Item as="a" href="https://github.com/mtaketani113/react-tools">
+            <Icon name='github' />
+          </List.Item>
+        </List>
+
       </Container>
     </Segment>
   );
